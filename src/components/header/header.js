93c734e737c721dcee3b14d02872737853cad18a
Joacim Breiler <joacim@breiler.com>
2023-03-01 19:08:38 +0100

    Fixed typo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
-import { Status } from "../../app";
 import * as classes from "./header.module.css";
 
-const logoUrl = new URL("../../assets/fluidnc.svg", import.meta.url);
+const logoUrl = new URL("../../assets/logo.svg", import.meta.url);
 
 const Header = () => {
   return (
-    <nav className={classes.nav + " navbar navbar-expand-lg navbar-dark bg-dark"}>
+    <nav
+      className={classes.nav + " navbar navbar-expand-lg navbar-dark bg-dark"}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img className={classes.logo} src={logoUrl} alt="logo" />

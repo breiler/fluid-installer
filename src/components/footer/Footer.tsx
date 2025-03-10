39ca9e1c61e10df2dedbe5852a26d7a45bf51bce
Joacim Breiler <joacim@breiler.com>
2025-03-10 12:55:59 +0100

    Add translatation link to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faBook, faExternalLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.scss";
@@ -13,22 +13,36 @@ const Footer = () => {
         <footer className="footer page-footer font-small blue pt-4">
             <ul className="list-unstyled text-start">
                 <li>
-                    <a href="http://wiki.fluidnc.com/">
+                    <a href="http://wiki.fluidnc.com/" target="_blank" rel="noreferrer">
                         {" "}
                         <FontAwesomeIcon icon={faBook as IconDefinition} />{" "}
                         {t("footer.documentation")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/bdring/FluidNC">
+                    <a href="https://github.com/bdring/FluidNC" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={faGithub as IconDefinition} />{" "}
                         FluidNC
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/breiler/fluid-installer">
+                    <a
+                        href="https://github.com/breiler/fluid-installer"
+                        target="_blank" rel="noreferrer"
+                    >
                         <FontAwesomeIcon icon={faGithub as IconDefinition} />{" "}
                         Installer
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://crowdin.com/project/fluid-installer"
+                        target="_blank" rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faExternalLink as IconDefinition}
+                        />{" "}
+                        {t("header.translate")}
                     </a>
                 </li>
             </ul>

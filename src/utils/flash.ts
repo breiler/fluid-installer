528354dec9c5b1b797882546307f3c225195f868
Joacim Breiler <joacim@breiler.com>
2025-11-24 18:53:32 +0100

    Attempt to do add a custom reset

--- src/utils/flash.ts
+++ src/utils/flash.ts
@@ -1,4 +1,11 @@
-import { ESPLoader, FlashOptions, LoaderOptions, Transport } from "esptool-js";
+import {
+    ClassicReset,
+    ESPLoader,
+    FlashOptions,
+    LoaderOptions,
+    ResetConstructors,
+    Transport
+} from "esptool-js";
 import CryptoJS from "crypto-js";
 import { FlashProgress } from "../services/FlashService";
 import { NativeSerialPort } from "./serialport/typings";
@@ -9,6 +16,39 @@ export type FlashFile = {
     fileName: string;
     data: Uint8Array;
     address: number;
+};
+
+/**
+ * Custom reset sequence
+ */
+const resetConstructors: ResetConstructors = {
+    classicReset: (transport: Transport, resetDelay: number) => {
+        const classicReset = new ClassicReset(transport, resetDelay);
+
+        // Override the reset function
+        classicReset.reset = async () => {
+            const serialDevice: NativeSerialPort = transport.device;
+
+            // D0|R1|W100|D1|R0|W50|D0
+            await serialDevice.setSignals({
+                dataTerminalReady: false,
+                requestToSend: true
+            });
+            await new Promise((r) => setTimeout(r, 100));
+            await serialDevice.setSignals({
+                dataTerminalReady: true,
+                requestToSend: false
+            });
+            await new Promise((r) => setTimeout(r, 50));
+            await serialDevice.setSignals({
+                dataTerminalReady: false,
+                requestToSend: false
+            });
+            return Promise.resolve();
+        };
+
+        return classicReset;
+    }
 };
 
 export const flashDevice = async (
@@ -36,7 +76,8 @@ export const flashDevice = async (
         const loaderOptions = {
             transport,
             baudrate: baud,
-            terminal: terminal
+            terminal: terminal,
+            resetConstructors: resetConstructors
         } as LoaderOptions;
         loader = new ESPLoader(loaderOptions);
         await loader!.main();

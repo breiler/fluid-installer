877625ee2ec92d784fc1e57c2d85ebffada07466
Joacim Breiler <joacim@breiler.com>
2025-11-24 19:04:52 +0100

    Add logging

--- src/utils/flash.ts
+++ src/utils/flash.ts
@@ -27,6 +27,7 @@ const resetConstructors: ResetConstructors = {
 
         // Override the reset function
         classicReset.reset = async () => {
+            console.log("Using classic reset sequence");
             const serialDevice: NativeSerialPort = transport.device;
 
             // D0|R1|W100|D1|R0|W50|D0
